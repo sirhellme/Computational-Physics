d5bd837548cc2e4bfa52aaadf8a7a9b7969d51ab
sirhellme <sirhellme@gmail.com>
2026-09-09 04:59:05 +0800

    added files

--- osilasi_teredam.py
+++ osilasi_teredam.py
@@ -1,33 +1,3 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# # omega^2
-# Osq = 0.5
-# #langkah
-# h = 0.2
-# # grid
-# t = np.arange(0,100,h)
-# # nilai awal
-# x0 = 3.
-# v0 = 0.
-# # kecepatan v1
-# v1 = v0 - Osq*x0*h
-# # posisi x1
-# x1 = x0 + v1*h
-
-# # daftar posisi
-# x = [x0,x1]
-
-# for i in range(2, len(t)):
-#   x.append(2*x[i-1] - x[i-2] - Osq*h*h*x[i-1])
-
-# print(len(t))
-# print(len(x))
-
-# plt.plot(t,x)
-# plt.xlabel('Time (s)')
-# plt.ylabel('Position (m)')
-# plt.show()
-
 # Model osilasi teredam
 # mx+bx+kx=0
 import numpy as np
---
+++ population_r_cons.py
@@ -0,0 +1,42 @@
+# population_r_cons.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#konstanta
+r = 0.2
+#
+
+# timestep
+h= 0.2 # dalam bulan
+
+# waktu-> grid
+t = np.arange(0,12,h)
+
+
+#Nilai awal
+N0 = 2
+Nh = N0 + N0*r*h/2  # beda maju
+
+# daftar N dan N 1/2
+N = [N0]
+Nhalf = [Nh]
+
+for i in range(len(t)-1):
+    N.append(N[i] + h*r*Nhalf[i])
+    Nhalf.append(Nhalf[i] + h*r*N[i+1])
+
+Nexact = N0*np.exp(r*t)
+
+#perhitungan error:
+error = np.sqrt(np.mean(np.square(Nexact-N)))
+print("Error = ", error)
+
+plt.plot(t,N, label='FD')
+plt.plot(t,Nexact, label = 'Exact')
+plt.legend()
+plt.xlim([0,12])
+plt.ylim([0,25])
+plt.xlabel('Time (months)')
+plt.ylabel('Population')
+plt.show()

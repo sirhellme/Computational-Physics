796068a127c9157e76b4d284236addfb0c9f9f6f
sirhellme <sirhellme@gmail.com>
2026-09-09 10:46:01 +0800

    fixing the code

--- Modul Osilasi/osilasi_teredam.py
+++ Modul Osilasi/osilasi_teredam.py
@@ -3,42 +3,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Parameter
-m = 1.0
-b = 0.2
-k = 0.5
+# parameter
+m = 1.0  # massa (kg)
+b = 0.2  # koefisien redaman (kg/s)
+k = 0.5  # konstanta pegas (N/m)
 
-h = 0.2
-t = np.arange(0, 100, h)
+osq = k / m  # frekuensi alami kuadrat (rad^2/s^2)
+gamma = b / m  # koefisien redaman (1/s)
 
-# Kondisi awal
-x0 = 3.0
-v0 = 0.0
+h = 0.01  # langkah waktu (s)
+t = np.arange(0, 100, h)  # vektor waktu (s)
 
-# Array
-x = [x0]
-v = [v0]
+# kondisi awal
+x0 = 3.0  # posisi awal (m)
+v0 = 0.0  # kecepatan awal (m/s)
 
-# Iterasi
-for i in range(len(t) - 1):
+# mencari v1
+a0 = -gamma * v0 - osq * x0  # percepatan awal (m/s^2)
 
-    # percepatan
-    a = -(b/m)*v[i] - (k/m)*x[i]
+v1 = v0 + a0 * h  # kecepatan pada langkah pertama (m/s)
 
-    # kecepatan baru
-    v_new = v[i] + a*h
+# mencari x1
+x1 = x0 + v0 * h  # posisi pada langkah pertama (m)
 
-    # posisi baru
-    x_new = x[i] + v_new*h
+# daftar posisi
+x = [x0, x1]
 
-    # simpan
-    v.append(v_new)
-    x.append(x_new)
+# finite difference method
+for i in range(2, len(t)):
+    a = -gamma * v1 - osq * x1  # percepatan (m/s^2)
+    xi = x[i - 1] + v1 * h  # posisi (m)
+    xim1 = x[i - 2] + v1 * h  # posisi sebelumnya (m)
+    vi = (xi - xim1) / h  # kecepatan (m/s)
+    
+    # finite difference formula
+    xbaru = 2 * xi - xim1 - gamma * vi * h**2 - osq * xi * h**2 # posisi baru (m)
 
-# Plot
+    x.append(xbaru)  # menambahkan posisi baru ke daftar
+
+# plot hasil
 plt.plot(t, x)
-plt.xlabel('Time (s)')
-plt.ylabel('Position (m)')
+
 plt.title('Osilasi Teredam')
+plt.xlabel('Waktu (s)')
+plt.ylabel('Posisi (m)')
 plt.grid()
 plt.show()

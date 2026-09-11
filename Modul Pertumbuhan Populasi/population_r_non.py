f7ff695bd3b5d770ac83ae0ba3c31008503f23af
sirhellme <sirhellme@gmail.com>
2026-09-12 04:56:42 +0800

    menambahkan model ancaman dan bencana random

--- Modul Pertumbuhan Populasi/population_r_non.py
+++ Modul Pertumbuhan Populasi/population_r_non.py
@@ -1,32 +1,55 @@
 # for nonconstant r
+
 # Model logistik : r = r0(1-N/M)
-# Model linier: r = r0(1/(1+N))
-# Model dengan ancaman: r = r0 bla bla bla
+# Model linier   : r = r0(1/(1+N))
+# Model dengan ancaman : r = r0(1-N/M) - d
+
 import numpy as np
 import matplotlib.pyplot as plt
-# timestep
+
+# parameter
 h= 0.01 # dalam bulan
-r0 = 1
-M = 20
+r0 = 1 # laju pertumbuhan maksimum
+M = 20 # kapasitas lingkungan
+d = 0.05 # kematian akibat ancaman 
+
+# parameter bencana
+p_bencana = 0.1 # probabilitas bencana
+D = 0.2 # dampak bencana (misal 20% populasi hilang)
 
 # waktu
 t = np.arange(0,12,h)
 
-#Nilai awal
+# Nilai awal
 N0 = 10
-#Nh = N0 +  N0/(1+N0)*h/2
-Nh = N0 + r0*(1-N0/M)*N0*h/2
 
-N = [N0]
+# N pada half step
+Nh = N0 + (r0*(1-N0/M)-d)*N0*h/2
+
+# Inisialisasi array untuk menyimpan nilai populasi
+N = [N0] 
 Nhalf = [Nh]
 
+# Loop untuk menghitung populasi pada setiap timestep
 for i in range(len(t)-1):
-    #N.append(N[i] + h*Nhalf[i]/(1+Nhalf[i]))
-    #Nhalf.append(Nhalf[i] + h*N[i+1]/(1+N[i+1]))
-    N.append(N[i] + h*r0*(1-N[i]/M)*Nhalf[i])
-    Nhalf.append(Nhalf[i] + h*r0*(1-Nhalf[i]/M)*N[i+1])
+    # laju pertumbuhan pada N[i]
+    r = r0*(1-N0/M)-d
 
-plt.plot(t,N, label='logistik')
+    # N(i+1)
+    N.append(N[i] + h*r*Nhalf[i])
+
+    # cek apakah terjadi bencana
+    if np.random.random() < p_bencana:
+        N[i+1] *= (1-D)  # populasi berkurang akibat bencana
+
+    # laju pertumbuhan pada Nhalf[i]
+    r_half = r0*(1-Nhalf[i]/M) - d
+
+    # Nhalf(i+1)
+    Nhalf.append(Nhalf[i] + h*r_half*N[i+1])
+
+# Plotting
+plt.plot(t,N, label='dengan ancaman')
 plt.xlim([0,12])
 plt.ylim([0,25])
 plt.legend()
